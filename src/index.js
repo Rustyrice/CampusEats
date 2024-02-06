e19a5a4bd1f099b20f1c8b3e1008b0b3c0b46dcc
Rustyrice <89965863+Rustyrice@users.noreply.github.com>
2024-02-06 22:53:37 +0000

    fixed file structure + integrated supabase & tailwind

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './index.css'
+
+// styles
+import 'tailwindcss/tailwind.css'
+
+// pages
+import LandingPage from './pages/Landing'
+import AuthPage from './pages/Auth'
+import HomePage from './pages/Home'
+import DashboardPage from './pages/Dashboard'
+import ErrorPage from './ErrorPage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/error" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+)
---
+++ src/pages/Landing.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Landing = () => {
+  return <div>hello</div>
+}
+
+export default Landing
