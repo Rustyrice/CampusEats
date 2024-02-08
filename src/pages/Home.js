d83ab7b4039dabba673debc15556dd27501dda9b
Rustyrice <89965863+Rustyrice@users.noreply.github.com>
2024-02-08 14:08:15 +0000

    initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 // components
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export const Home = () => {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
