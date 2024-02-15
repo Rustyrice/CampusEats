f7f747bb49a380d60d0cbcf6ce9eb92c58e141e6
Leonid <109795951+ekdaZ@users.noreply.github.com>
2024-02-15 09:58:00 +0000

    minor update
    fills two password entries the same
    no extra error handling

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -82,16 +82,23 @@ return (
       </label>
       <input onChange={handleEmail} value={email} className="input" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Example@gmail.com"/>
     </div>
-    <div class="mb-6">
+    <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
         Password
       </label>
       <input onChange={handlePassword} className="input" 
 value={password} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
     </div>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Please enter Password again
+      </label>
+      <input onChange={handlePassword} className="input" 
+value={password} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+    </div>
     <div class="flex items-center justify-between">
       <button onClick={handleSubmit} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Sign In
+        Sign Up
       </button>
     </div>
     <div class="text-red-500 text-xs italic">
@@ -99,14 +106,8 @@ value={password} class="shadow appearance-none border rounded w-full py-2 px-3 t
       {successMessage()}
       </div>
   </form>
-  {/* <p class="text-center text-gray-500 text-xs">
-    &copy;2020 Acme Corp. All rights reserved.
-  </p> */}
 </div>
 </div>
 
 );
 }
-
-// pull test
-// pull test 2
