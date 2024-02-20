7af0e01e4a453b2ae1ce084fa65910a5b8fd8f80
Sina <saatchisina@gmail.com>
2024-02-20 20:01:33 +0000

    Added Backend Functionality For Dietary Selection

---
+++ src/services/userPreferenceService.js
@@ -0,0 +1,33 @@
+import { supabase } from "../config/client";
+
+const getPreferences = async (userID) => {
+  const { data, error } = await supabase
+    .from("users")
+    .select("allergens")
+    .eq("id", userID);
+
+  if (error) {
+    console.error(error);
+  }
+  return data;
+};
+
+const submitPreferences = async (preferences, userID) => {
+  const { data, error } = await supabase
+    .from("users")
+    .update({ allergens: [preferences] })
+    .eq("id", userID)
+    .select();
+
+  console.log(await getPreferences(userID));
+
+  if (error) {
+    console.error(error);
+  }
+  return data;
+};
+
+export default {
+  getPreferences,
+  submitPreferences,
+};
