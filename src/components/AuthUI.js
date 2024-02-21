7e05beab156f563548ce5681b45c92e3a143a924
jmorada <jm3316@bath.ac.uk>
2024-02-21 16:54:11 +0000

    front end for auth and relative css

--- src/components/AuthUI.js
+++ src/components/AuthUI.js
@@ -3,6 +3,31 @@ import { useNavigate } from "react-router-dom";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { supabase } from "../config/client";
+const customTheme = {
+  default: {
+    colors: {
+      brand: 'hsl(153 60.0% 53.0%)',
+      brandAccent: 'hsl(154 54.8% 45.1%)',
+      brandButtonText: 'white',
+    },
+  },
+  dark: {
+  
+    colors: {
+      brandButtonText: 'white',
+      defaultButtonBackground: '#2e2e2e',
+      defaultButtonBackgroundHover: '#3e3e3e',
+      
+    },
+  },
+  evenDarker: {
+    colors: {
+      brandButtonText: 'white',
+      defaultButtonBackground: '#1e1e1e',
+      defaultButtonBackgroundHover: '#2e2e2e',
+    },
+  },
+}
 
 const AuthUI = () => {
   const navigate = useNavigate();
@@ -20,17 +45,27 @@ const AuthUI = () => {
   }, [navigate]);
 
   return (
-    <Auth 
-      supabaseClient={supabase}
-      providers={["google", "github", "discord"]}
-      // controls whether to display only social providers
-      // onlyThirdPartyProviders
-      redirectTo="http://localhost:3000/Dashboard"
-      // comes with preconfigured themes, can add custom themes
-      appearance={{ theme: ThemeSupa }}
-      // controls how to display the social provider icons
-      socialLayout="horizontal"
-    />
+    <div className="auth-container">
+      <Auth 
+        supabaseClient={supabase}
+        // controls whether to display only social providers
+        providers={["google", "github", "discord"]}
+        // onlyThirdPartyProviders
+        redirectTo="http://localhost:3000/Dashboard"
+        // comes with preconfigured themes, can add custom themes
+        theme="dark"
+        appearance={{
+          theme: customTheme,
+          className: {
+            container: "provider-container",
+            button: "auth-button"
+          }
+         }}
+        // controls how to display the social provider icons
+        socialLayout="horizontal"
+        
+      />
+    </div>
   );
 };
 
