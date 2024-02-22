8889c4e682afffb9f5204ff06730b627e5fcddd7
vrushaliashok <84222495+vrushaliashok@users.noreply.github.com>
2024-02-22 13:17:24 +0000

    Dashboard update
    Updated dashboard to include checkboxes.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import RecipeCard from '../components/RecipeCard';
 import PopUp from '../components/PopUp';
 import ingredientService from "../services/ingredientService";
+import SelectionBox from "../components/SelectionBox";
 
 export default function Dashboard() {
   const [meals, setMeals] = useState([]);
@@ -10,6 +11,13 @@ export default function Dashboard() {
   const [newMeal, setNewMeal] = useState({ name: "", description: "" });
   const [addButton, setAddButton] = useState(false);
   
+  const updatePreferences = (val) => {
+    setNewMeal(prevState => ({
+      ...prevState,
+      [val]: !prevState[val],
+    }));
+  };
+
   const getMeals = async () => {
     try {
       const meals = await ingredientService.fetchMeals();
@@ -35,12 +43,11 @@ export default function Dashboard() {
   const addNewMeal = async () => {
     try {
       await ingredientService.addMeal(newMeal);
-      setMeals([...meals, newMeal]); // Update meals state with the new meal
-      setAddButton(false); // Close the popup after adding the meal
-      setNewMeal({ name: "", description: "" }); // Clear the newMeal state
+      setMeals([...meals, newMeal]); 
+      setAddButton(false); 
+      setNewMeal({ name: "", description: "" }); 
     } catch (error) {
       console.error("Failed to add meal:", error);
-      // Handle error if needed
     }
   };
 
@@ -89,6 +96,7 @@ export default function Dashboard() {
           </div>
       
           <PopUp trigger={addButton} onClose={() => setAddButton(false)}> 
+          <div className = "w-96 h-96 overflow-y-auto"> 
             <form onSubmit={handleSubmit}>
               <input
                 type="text"
@@ -103,8 +111,86 @@ export default function Dashboard() {
                 onChange={handleDescriptionChange}
                 className="w-full h-20 px-4 py-2 rounded-lg border-2 border-gray-300 focus:border-green-500 focus:outline-none mt-2"
               />
-              <button type="submit" className="bg-green-400 hover:bg-green-700 text-white py-2 px-4 rounded-lg"> Add Meal </button>
+              <div className="flex flex-col justify-center ml-5 mr-5 content-center">
+                <SelectionBox
+                  desc="Halal"
+                  onClick={() => updatePreferences("halal")}
+                  checked={newMeal.halal}
+                />
+                <SelectionBox
+                  desc="Vegetarian"
+                  onClick={() => updatePreferences("vegetarian")}
+                  checked={newMeal.vegetarian}
+                />
+                <SelectionBox
+                  desc="Vegan"
+                  onClick={() => updatePreferences("vegan")}
+                  checked={newMeal.vegan}
+                />
+                <SelectionBox
+                desc="Lupin"
+                checked={newMeal.lupin}
+                onClick={() => updatePreferences("lupin")}
+                />
+                <SelectionBox
+                  desc="Soya"
+                  checked={newMeal.soya}
+                  onClick={() => updatePreferences("soya")}
+                />
+                <SelectionBox
+                  desc="Egg"
+                  checked={newMeal.egg}
+                  onClick={() => updatePreferences("egg")}
+                />
+                <SelectionBox
+                  desc="Milk"
+                  checked={newMeal.milk}
+                  onClick={() => updatePreferences("milk")}
+                />
+                <SelectionBox
+                  desc="Fish"
+                  checked={newMeal.fish}
+                  onClick={() => updatePreferences("fish")}
+                />
+                <SelectionBox
+                  desc="Crustaceans"
+                  checked={newMeal.crustaceans}
+                  onClick={() => updatePreferences("crustaceans")}
+                />
+                <SelectionBox
+                  desc="Molluscs"
+                  checked={newMeal.molluscs}
+                  onClick={() => updatePreferences("molluscs")}
+                />
+                <SelectionBox
+                  desc="Mustard"
+                  checked={newMeal.mustard}
+                  onClick={() => updatePreferences("mustard")}
+                />
+                <SelectionBox
+                  desc="Celery"
+                  checked={newMeal.celery}
+                  onClick={() => updatePreferences("celery")}
+                />
+                <SelectionBox
+                  desc="Peanuts"
+                  checked={newMeal.peanuts}
+                  onClick={() => updatePreferences("peanuts")}
+                />
+                <SelectionBox
+                  desc="Sesame Seeds"
+                  checked={newMeal.sesame_seeds}
+                  onClick={() => updatePreferences("sesame_seeds")}
+                />
+                <SelectionBox
+                  desc="Sulphur Dioxide"
+                  checked={newMeal.sulphur_dioxide}
+                  onClick={() => updatePreferences("sulphur_dioxide")}
+                />
+              </div>
+              <button type="submit" className="bg-green-400 hover:bg-green-700 text-white py-2 px-4 rounded-lg mt-2"> Add Meal </button>
             </form>
+            </div>
           </PopUp>
 
           <div className='recipes-container gap-5 ml-1/4'>
