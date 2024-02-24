89507e4a6f30840c4d5a6e8885ea272cf5148714
Rustyrice <89965863+Rustyrice@users.noreply.github.com>
2024-02-24 11:57:09 +0000

    made changes to landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,70 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-// import the background image for the landing page
-import backgroundImage from '../../src/pages/background.jpg';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import backgroundImage from '../../src/assets/landing_background_image.jpg'
 
 const Landing = () => {
-  // container of the landing page
-  const containerStyle = {
-    backgroundImage: `url(${backgroundImage})`, // set the background image
-    backgroundSize: 'cover', // ensure background image covers entire container
-    backgroundPosition: 'center', // centres image
-    height: '100vh', // full viewport height
-    // adapts for mobile view
-    display: 'flex', 
-    flexDirection: 'column',
-    justifyContent: 'flex-start', // ??
-    alignItems: 'flex-start', 
-    color: 'white', 
-    padding: '5vw', 
-  };
-
-  // signup button
-  const buttonStyle = {
-    display: 'inline-block', 
-    padding: '0.8rem 1.5rem',
-    fontSize: '1rem', 
-    fontWeight: 'bold', 
-    lineHeight: '1.5',
-    color: 'white',
-    transition: 'background-color 0.3s ease',
-    transform: 'translateY(0)', 
-    backgroundColor: '#34D399', 
-    borderRadius: '0.5rem', 
-    marginTop: '2vw', 
-  };
-
-  // heading text
-  const headingStyle = {
-    fontSize: '3rem', 
-    marginBottom: '0.7vw',
-  };
-
-  // description
-  const descriptionStyle = {
-    fontSize: '0.8rem', 
-    marginTop: '2vw', // margin based on viewport width
-  };
-
   return (
-    <div style={containerStyle}>
-      <div className="text-gray-800 text-5xl font-bold cursor" style={headingStyle}>
-        Discover <br />Mindful Eating
-      </div>
+    <div
+      className="relative bg-cover bg-center h-screen flex flex-col justify-start items-start text-white p-5"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-30 w-full h-full p-5">
+        <div className="relative z-10 text-3xl sm:text-5xl font-bold mb-7 mt-10 sm:mt-5">
+          Discover <br />
+          Mindful Eating
+        </div>
 
-      <div className="text-gray-800 text-1xl font cursor" style={descriptionStyle}>
-        SAY GOODBYE TO THE STRESS OF FINDING <br />
-        SAFE MEALS ON CAMPUS AND EMBARK ON A <br />
-        CULINARY JOURNEY THAT ALIGNS PERFECTLY<br />
-        WITH YOUR DIETARY NEEDS
-      </div>
+        <div className="text-sm sm:text-lg mt-4">
+          Say goodbye to the stress of finding <br />
+          safe meals on campus and embark on a <br />
+          culinary journey that aligns perfectly
+          <br />
+          with your dietary needs.
+        </div>
 
-      <Link to="/auth" style={buttonStyle}>
-        Sign Up
-      </Link>
+        <Link
+          to="/auth"
+          className="relative z-10 inline-block bg-green-500 text-white py-2 px-4 mt-6 sm:mt-4 font-bold rounded"
+        >
+          Get Started
+        </Link>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing
