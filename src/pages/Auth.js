041010b6871ddc849e84a1174db3669d3bddea28
Rustyrice <89965863+Rustyrice@users.noreply.github.com>
2024-03-02 17:02:24 +0000

    minor frontend changes

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -2,17 +2,15 @@ import React from 'react'
 
 import AuthUi from '../components/AuthUI'
 
-
 const Auth = () => {
   return (
     <>
       <div className="flex flex-col items-center">
-        <h2 className =" p-12 my-28 font-bold text-4xl border-double border-2 border-green-500 ">
+        <h2 className="p-12 mt-7 md:mt-10 mb-7 md:mb-10 font-bold text-4xl border-double border-2 border-green-500 ">
           CampusEats
         </h2>
-        <AuthUi /> 
+        <AuthUi />
       </div>
-      
     </>
   )
   //<div>hello from auth</div>
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,6 +3,7 @@ import SelectionBox from '../components/SelectionBox'
 import { Link } from 'react-router-dom'
 import { supabase } from '../config/client'
 import userPreferenceService from '../services/userPreferenceService'
+import Navbar from '../components/Navbar'
 
 const Settings = () => {
   const [user, setUser] = useState(null)
@@ -45,113 +46,108 @@ const Settings = () => {
   if (preferences)
     return (
       <div>
-        {/* LOGO */}
-        <p className="text-center font-bold text-2xl mt-10">CampusEats</p>
-
-        {/* PROGRESS BAR
-      <div className="mt-5 flex-start flex h-2.5 w-full overflow-hidden rounded-full bg-blue-gray-50 font-sans text-xs font-medium">
-        <div className="flex items-center justify-center w-1/2 h-full overflow-hidden text-white break-all bg-green-500 rounded-full"></div>
-      </div> */}
-
-        <p className="text-center text-gray-700 text-xl mb-4 mt-5">
-          Greetings, {username}!
-        </p>
-        <p className="text-center text-gray-700 text-l mb-4 mt-5">
-          Customize your dietary preferences below and click submit to save.
-        </p>
-        {/* CHECKLIST */}
-        <div className="flex flex-col justify-center ml-5 mr-5 content-center">
-          <SelectionBox
-            desc="Halal"
-            onClick={() => updatePreferences('halal')}
-            checked={preferences.halal}
-          />
-          <SelectionBox
-            desc="Vegetarian"
-            onClick={() => updatePreferences('vegetarian')}
-            checked={preferences.vegetarian}
-          />
-          <SelectionBox
-            desc="Vegan"
-            onClick={() => updatePreferences('vegan')}
-            checked={preferences.vegan}
-          />
-          <p className="text-center text-gray-700 text-l mb-4 mt-5">
-            Ticked boxes indicate the allergens you are allergic to.
+        <Navbar />
+        <div className="py-12">
+          <p className="text-center text-gray-700 text-xl mb-4 mt-5">
+            Greetings, {username}!
           </p>
-          <SelectionBox
-            desc="Lupin"
-            checked={preferences.lupin}
-            onClick={() => updatePreferences('lupin')}
-          />
+          <p className="text-center text-gray-700 text-l mb-4 mt-5">
+            Customize your dietary preferences below and click submit to save.
+          </p>
+          {/* CHECKLIST */}
+          <div className="flex flex-col justify-center ml-5 mr-5 content-center">
+            <SelectionBox
+              desc="Halal"
+              onClick={() => updatePreferences('halal')}
+              checked={preferences.halal}
+            />
+            <SelectionBox
+              desc="Vegetarian"
+              onClick={() => updatePreferences('vegetarian')}
+              checked={preferences.vegetarian}
+            />
+            <SelectionBox
+              desc="Vegan"
+              onClick={() => updatePreferences('vegan')}
+              checked={preferences.vegan}
+            />
+            <p className="text-center text-gray-700 text-l mb-4 mt-5">
+              Ticked boxes indicate the allergens you are allergic to.
+            </p>
+            <SelectionBox
+              desc="Lupin"
+              checked={preferences.lupin}
+              onClick={() => updatePreferences('lupin')}
+            />
 
-          <SelectionBox
-            desc="Soya"
-            checked={preferences.soya}
-            onClick={() => updatePreferences('soya')}
-          />
-          <SelectionBox
-            desc="Egg"
-            checked={preferences.egg}
-            onClick={() => updatePreferences('egg')}
-          />
-          <SelectionBox
-            desc="Milk"
-            checked={preferences.milk}
-            onClick={() => updatePreferences('milk')}
-          />
-          <SelectionBox
-            desc="Fish"
-            checked={preferences.fish}
-            onClick={() => updatePreferences('fish')}
-          />
-          <SelectionBox
-            desc="Crustaceans"
-            checked={preferences.crustaceans}
-            onClick={() => updatePreferences('crustaceans')}
-          />
-          <SelectionBox
-            desc="Molluscs"
-            checked={preferences.molluscs}
-            onClick={() => updatePreferences('molluscs')}
-          />
-          <SelectionBox
-            desc="Mustard"
-            checked={preferences.mustard}
-            onClick={() => updatePreferences('mustard')}
-          />
-          <SelectionBox
-            desc="Celery"
-            checked={preferences.celery}
-            onClick={() => updatePreferences('celery')}
-          />
-          <SelectionBox
-            desc="Peanuts"
-            checked={preferences.peanuts}
-            onClick={() => updatePreferences('peanuts')}
-          />
-          <SelectionBox
-            desc="Sesame Seeds"
-            checked={preferences.sesame_seeds}
-            onClick={() => updatePreferences('sesame_seeds')}
-          />
-          <SelectionBox
-            desc="Sulphur Dioxide"
-            checked={preferences.sulphur_dioxide}
-            onClick={() => updatePreferences('sulphur_dioxide')}
-          />
+            <SelectionBox
+              desc="Soya"
+              checked={preferences.soya}
+              onClick={() => updatePreferences('soya')}
+            />
+            <SelectionBox
+              desc="Egg"
+              checked={preferences.egg}
+              onClick={() => updatePreferences('egg')}
+            />
+            <SelectionBox
+              desc="Milk"
+              checked={preferences.milk}
+              onClick={() => updatePreferences('milk')}
+            />
+            <SelectionBox
+              desc="Fish"
+              checked={preferences.fish}
+              onClick={() => updatePreferences('fish')}
+            />
+            <SelectionBox
+              desc="Crustaceans"
+              checked={preferences.crustaceans}
+              onClick={() => updatePreferences('crustaceans')}
+            />
+            <SelectionBox
+              desc="Molluscs"
+              checked={preferences.molluscs}
+              onClick={() => updatePreferences('molluscs')}
+            />
+            <SelectionBox
+              desc="Mustard"
+              checked={preferences.mustard}
+              onClick={() => updatePreferences('mustard')}
+            />
+            <SelectionBox
+              desc="Celery"
+              checked={preferences.celery}
+              onClick={() => updatePreferences('celery')}
+            />
+            <SelectionBox
+              desc="Peanuts"
+              checked={preferences.peanuts}
+              onClick={() => updatePreferences('peanuts')}
+            />
+            <SelectionBox
+              desc="Sesame Seeds"
+              checked={preferences.sesame_seeds}
+              onClick={() => updatePreferences('sesame_seeds')}
+            />
+            <SelectionBox
+              desc="Sulphur Dioxide"
+              checked={preferences.sulphur_dioxide}
+              onClick={() => updatePreferences('sulphur_dioxide')}
+            />
 
-          {/* NAVIGATION BUTTONS */}
-          <div className="flex justify-center content-center mb-5 mt-5">
-            <Link
-              to="/home"
-              className="hover:bg-green-400 rounded-md bg-green-500 p-5 px-10 text-white"
-              onClick={() => {
-                userPreferenceService.submitPreferences(preferences, user.id)
-              }}
-            >
-              Save Preferences
-            </Link>
+            {/* NAVIGATION BUTTONS */}
+            <div className="flex justify-center content-center mb-5 mt-5">
+              <Link
+                to="/home"
+                className="hover:bg-green-400 rounded-md bg-green-500 p-5 px-10 text-white"
+                onClick={() => {
+                  userPreferenceService.submitPreferences(preferences, user.id)
+                }}
+              >
+                Save Preferences
+              </Link>
+            </div>
           </div>
         </div>
       </div>
